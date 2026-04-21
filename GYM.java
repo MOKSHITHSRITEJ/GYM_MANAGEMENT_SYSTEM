ca9a315d67a4c717958dc1e455c66011a223a976
MOKSHITH <mokshithsritejposimsetty@gmail.com>
2026-04-21 16:16:34 +0530

    first commit

---
+++ GYM.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class GYM{
+     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER YOUR NAME: ");
+        String Name=sc.nextLine();
+        System.out.print("ENTER YOUR AGE: ");
+        int age=sc.nextInt();
+
+    }
+}
+
